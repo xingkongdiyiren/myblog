02ab354fb4e427004971d50b525f68c8f51cde00
zhouhengzhe <1871229316@qq.com>
2023-04-15 03:53:43 +0800

    feat：去掉代理

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -119,13 +119,6 @@ export default {
         placeholder: '请输入关键词',
         buttonText: '搜索',
     },
-    server: {
-        hmr: {overlay: false}, // 禁用或配置 HMR 连接 设置 server.hmr.overlay 为 false 可以禁用服务器错误遮罩层
-        // 服务配置
-        open: false, // 类型： boolean | string在服务器启动时自动在浏览器中打开应用程序；
-        cors: false, // 类型： boolean | CorsOptions 为开发服务器配置 CORS。默认启用并允许任何源
-        host: '0.0.0.0', // 支持从IP启动访问
-    },
     head: [
         ['meta', {name: 'theme-color', content: '#3c8772'}],
         [
